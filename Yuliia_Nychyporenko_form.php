e3997b48c618010d796294c2c56bc89938fb34cf
yulianychyporenko <149169248+yulianychyporenko@users.noreply.github.com>
2023-11-25 15:51:12 +0100

    Add files via upload

---
+++ Yuliia_Nychyporenko_form.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link REL="stylesheet" TYPE="text/css"
+          HREF="styleform.css">
+    <title>Spracovanie konkurzu</title>
+</head>
+<body>
+<form class="transparent">
+    <div class="form-inner">
+    <h1>ďakujem za vaš ohlas!</h1>
+    <label><?php echo "$work"; ?></label>
+    <label><?php echo "$name $surname"; ?></label>
+    <label>Dátum narodenia: <?php echo "$age"; ?></label>
+    <label><?php echo "$name $surname"; ?></label>
+    <label><?php echo "$phone"; ?></label>
+    <label><?php echo "$email"; ?></label>
+    <label><?php echo "$vzd"; ?></label>
+    <label><?php echo "$web"; ?></label>
+    <h2>odpoveď očakávajte do 3 dní, prosím.</h2>
+    </div>
+</form>
+</body>
+</html>
